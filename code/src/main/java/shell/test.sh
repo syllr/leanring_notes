099c556081e0aa2996f7fb95b87c85eaa7a9ef94
yutao <shenyuanlaolarou@gmail.com>
2019-01-13 17:36:17 +0800

    update

--- code/src/main/java/shell/test.sh
+++ code/src/main/java/shell/test.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-a=1
-b=2
-
-if [ ${a} -eq ${b} ]; then
-    echo '${a} -eq ${b}：a等于b'
-else
-    echo '${a} -eq ${b}：a不等于b'
-fi
-
+while :
+do
+    echo -n "输入1到5之间的数字："
+    read aNum
+    case ${aNum} in
+        1) echo "echo ${aNum}"
+        ;;
+        *) echo "你输入的不是"
+        break
+        ;;
+    esac
+done
