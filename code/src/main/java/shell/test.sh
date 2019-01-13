a55358c529d17dea93c7e02dc85fe2a6b57bed84
yutao <shenyuanlaolarou@gmail.com>
2019-01-13 18:16:25 +0800

    添加let命令

--- code/src/main/java/shell/test.sh
+++ code/src/main/java/shell/test.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
-while :
-do
-    echo -n "输入1到5之间的数字："
-    read aNum
-    case ${aNum} in
-        1) echo "echo ${aNum}"
-        ;;
-        *) echo "你输入的不是"
-        break
-        ;;
-    esac
-done
+index=10*10
+echo $index
